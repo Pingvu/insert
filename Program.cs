f2d34d5bfc186f92f219c8f6ec81f1d2d1a30816
naal01002 <89377694+naal01002@users.noreply.github.com>
2022-02-15 14:00:31 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace insert
+{
+    class InsertionSort
+    {
+        void sort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    static void printArray(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+ 
+        Console.Write("\n");
+    }
+ 
+  
+    public static void Main()
+    {{int []arr = new int[8000];
+    Random rnd = new Random();
+    for (int i = 0; i < 8000; i++) {
+  arr[i] = rnd.Next(0,1000) ;
+}
+   Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+         stopwatch.Stop();
+        printArray(arr);
+        Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+    }
+}
+    }}
